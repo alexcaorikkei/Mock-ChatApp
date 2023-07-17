c24c5d5ab05e9d3e017baf212bd828cf9d4eb434
ngoc2k1 <englishcoffee9999@gmail.com>
2023-07-17 16:18:16 +0700

    fix conflict

--- app/src/main/java/com/example/baseproject/ui/home/messages/MessagesFragment.kt
+++ app/src/main/java/com/example/baseproject/ui/home/messages/MessagesFragment.kt
@@ -1,9 +1,11 @@
 package com.example.baseproject.ui.home.messages
 
+import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.baseproject.R
 import com.example.baseproject.databinding.FragmentMessagesBinding
+import com.example.baseproject.domain.model.Response
 import com.example.baseproject.navigation.AppNavigation
 import com.example.baseproject.ui.home.messages.adapter.MessageAdapter
 import com.example.core.base.fragment.BaseFragment
@@ -49,27 +51,5 @@ class MessagesFragment : BaseFragment<FragmentMessagesBinding, MessagesViewModel
         binding.swipeRefreshLayout.setOnRefreshListener {
             viewModel.searchMessages("")
         }
-
-        binding.rvMessages.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvMessages.adapter = MessageAdapter(listOf(
-            MessageModel(1, "Nguyen Van A", "Hello", "10:00", "", false),
-            MessageModel(2, "Nguyen Van B", "Hello", "10:00", "", true),
-            MessageModel(3, "Nguyen Van C", "Hello", "10:00", "", true),
-            MessageModel(4, "Nguyen Van D", "Hello", "10:00", "", false),
-            MessageModel(5, "Nguyen Van E", "Hello", "10:00", "", false),
-            MessageModel(6, "Nguyen Van F", "Hello", "10:00", "", true),
-            MessageModel(7, "Nguyen Van G", "Hello", "10:00", "", false),
-            MessageModel(8, "Nguyen Van H", "Hello", "10:00", "", true),
-            MessageModel(9, "Nguyen Van I", "Hello", "10:00", "", false),
-            MessageModel(10, "Nguyen Van J", "Hello", "10:00", "", true),
-            MessageModel(11, "Nguyen Van K", "Hello", "10:00", "", false),
-            MessageModel(12, "Nguyen Van L", "Hello", "10:00", "", true),
-            MessageModel(7, "Nguyen Van G", "Hello", "10:00", "", false),
-            MessageModel(8, "Nguyen Van H", "Hello", "10:00", "", true),
-            MessageModel(9, "Nguyen Van I", "Hello", "10:00", "", false),
-            MessageModel(10, "Nguyen Van J", "Hello", "10:00", "", true),
-            MessageModel(11, "Nguyen Van K", "Hello", "10:00", "", false),
-            MessageModel(12, "Nguyen Van L", "Hello", "10:00", "", true),
-        ))
     }
 }
