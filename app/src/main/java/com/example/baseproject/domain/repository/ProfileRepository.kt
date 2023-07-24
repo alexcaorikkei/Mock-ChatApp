25e649e084fd973ff0f9affec7d4bda02021a484
Cao Việt Đức <112269340+alexcao194@users.noreply.github.com>
2023-07-24 10:23:42 +0700

    profile -> realtime

--- app/src/main/java/com/example/baseproject/domain/repository/ProfileRepository.kt
+++ app/src/main/java/com/example/baseproject/domain/repository/ProfileRepository.kt
@@ -1,9 +1,10 @@
 package com.example.baseproject.domain.repository
 import android.net.Uri
+import androidx.lifecycle.MutableLiveData
 import com.example.baseproject.domain.model.Response
 import com.example.baseproject.domain.model.UserModel
 
 interface ProfileRepository {
     suspend fun updateProfile(user : UserModel, profilePictureUri: Uri?): Response<Boolean>
-    suspend fun getProfile(): Response<UserModel>
+    fun getProfile(): MutableLiveData<Response<UserModel>>
 }
